2df6ea54fd93efee3b7c7cf20ca5ed70f6b14536
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-10-28 14:08:48 +0430

    Finish the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import variables from './variable.module.scss';
+import Image from 'next/image';
+import pic from '../public/image/pic.webp';
 
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
-    </main>
+    <div className="flex flex-col lg:flex-row items-center lg:items-start pl-4 lg:pl-24 p-12 min-h-screen bg-gray-100">
+      <div className="mb-6 lg:mb-0 lg:mr-8 lg:p-6">
+        <Image
+          src={pic}
+          alt="landing picture"
+          width={205}
+          height={75}
+          priority
+        />
+      </div>
+      <div className="text-center lg:text-left">
+        <h1 className="text-2xl lg:text-4xl font-bold mb-4 lg:mb-8">An opportunity for<span className="text-[#FF7C24]"> sustainable living </span> and Development</h1>
+        <p className="text-base lg:text-lg mb-4 lg:mb-2">and the Exclusive Representative of NUUKO POWER Solar Brand in the Country</p>
+      </div>
+    </div>
   );
 }
+
+export default Home;
