b4292076015b1fbb77b923dc106c2a5fb3211a39
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-10-31 14:13:34 +0430

    Create the slider part

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
-import Image from 'next/image';
-import pic from '../public/image/pic.webp';
+import Page from '../app/Landing/page';
 
 function Home() {
   return (
-    <div className="flex flex-col lg:flex-row items-center lg:items-start pl-4 lg:pl-24 p-12 min-h-screen bg-gray-100">
-      <div className="mb-6 lg:mb-0 lg:mr-8 lg:p-6">
-        <Image
-          src={pic}
-          alt="landing picture"
-          width={300}
-          height={75}
-          priority
-        />
-      </div>
-      <div className="text-center lg:text-left p-12">
-        <h1 className="text-2xl lg:text-4xl font-bold mb-4 lg:mb-8">An opportunity for<span className="text-[#FF7C24]"> sustainable living </span> and Development</h1>
-        <p className="text-base lg:text-lg mb-4 lg:mb-2">and the Exclusive Representative of NUUKO POWER Solar Brand in the Country</p>
-      </div>
-    </div>
+   <div>
+    <Page />
+   </div>
   );
 }
 
